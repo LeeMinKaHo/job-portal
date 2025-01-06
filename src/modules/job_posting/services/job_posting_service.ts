3225cbfdecc7c73714c39fa6a2afae79a1c4275f
LeeMinKaHo <leminhkhoa5241@gmail.com>
2025-01-06 18:15:59 +0700

    CRUD job_posting

--- src/modules/job_posting/services/job_posting_service.ts
+++ src/modules/job_posting/services/job_posting_service.ts
@@ -1,6 +1,8 @@
-import { Injectable, Inject } from "@nestjs/common";
+import { Injectable, Inject, Body } from "@nestjs/common";
 import { job_posting } from "src/database/entities/job_posting";
-import { Repository } from "typeorm";
+import { DeleteResult, Repository } from "typeorm";
+import { create_job_posting_dto } from "../dtos/create_job-posting_dto";
+import { update_job_posting_dto } from "../dtos/update_job-posting_dto";
 
 
 @Injectable()
@@ -12,5 +14,39 @@ export class job_posting_service {
 
   async findAll(): Promise<job_posting[]> {
     return await this.job_posting_repository.find();
+  } 
+  async create(createJobPostingDto: create_job_posting_dto): Promise<job_posting> {
+    console.log(createJobPostingDto)
+    const newJobPosting = this.job_posting_repository.create();
+    Object.assign(newJobPosting, createJobPostingDto);
+    await this.job_posting_repository.save(newJobPosting)
+    return newJobPosting;
+  }
+  
+  async findOne(id: number): Promise<job_posting | null> {
+    const jobPosting = await this.job_posting_repository.findOne({ where: { id: id } });
+    return jobPosting || null; // Trả về null nếu không tìm thấy
+  }
+  
+  async update(update_job_posting_dto: update_job_posting_dto): Promise<job_posting | null> {
+    // Tìm bản ghi với ID được cung cấp
+    const existingJob = await this.findOne(update_job_posting_dto.id);
+  
+    if (!existingJob) {
+      // Nếu không tìm thấy, trả về null hoặc ném lỗi
+      throw new Error(`Job posting with ID ${update_job_posting_dto.id} not found`);
+    }
+  
+    // Cập nhật dữ liệu mới vào bản ghi tìm thấy
+    Object.assign(existingJob, update_job_posting_dto);
+  
+    // Lưu thay đổi
+    return await this.job_posting_repository.save(existingJob);
+  }  
+
+  async delete(id: number): Promise<DeleteResult> {
+    // Tìm và xóa bản ghi theo ID
+    const result = await this.job_posting_repository.update(id,{ is_Hidden:true});
+    return result;
   }
 }
